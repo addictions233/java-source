989288d410c5170688cf94b16edf7c57172ed5dd
wanjunjie <ogo1828wan@163.com>
2025-07-02 00:52:32 +0800

    学习mybatis-spring源码

--- mybatis-sources/mybatis-3.5.3/src/main/java/org/apache/ibatis/binding/MapperProxy.java
+++ mybatis-sources/mybatis-3.5.3/src/main/java/org/apache/ibatis/binding/MapperProxy.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2021 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.binding;
 
@@ -34,7 +34,7 @@ public class MapperProxy<T> implements InvocationHandler, Serializable {
 
   private static final long serialVersionUID = -6424540398559729838L;
   private static final int ALLOWED_MODES = MethodHandles.Lookup.PRIVATE | MethodHandles.Lookup.PROTECTED
-      | MethodHandles.Lookup.PACKAGE | MethodHandles.Lookup.PUBLIC;
+    | MethodHandles.Lookup.PACKAGE | MethodHandles.Lookup.PUBLIC;
   private static Constructor<Lookup> lookupConstructor;
   private final SqlSession sqlSession;
   private final Class<T> mapperInterface;
@@ -69,15 +69,11 @@ public class MapperProxy<T> implements InvocationHandler, Serializable {
    * @param proxy 代理对象
    * @param method:目标方法
    * @param args :目标对象参数
-   * @return:Object
-   * @exception:
    */
   @Override
   public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
     try {
-      /**
-       * 判断我们的方法是不是我们的Object类定义的方法，若是直接通过反射调用
-       */
+      // 判断我们的方法是不是我们的Object类定义的方法，例如:toString, hashCode等方法, 若是直接通过反射调用, 无需执行sql流程
       if (Object.class.equals(method.getDeclaringClass())) {
         return method.invoke(this, args);
       } else if (method.isDefault()) {   //是否接口的默认方法
@@ -92,20 +88,21 @@ public class MapperProxy<T> implements InvocationHandler, Serializable {
     /**
      * 真正的进行调用,做了二个事情
      * 第一步:把我们的方法对象封装成一个MapperMethod对象(带有缓存作用的)
+     * 一个mapper接口的method方法对应一个CRUD标签, 也就是一条sql语句
      */
     final MapperMethod mapperMethod = cachedMapperMethod(method);
     /**
-     *通过sqlSessionTemplate来调用我们的目标方法
+     * 通过sqlSessionTemplate来调用我们的目标方法
      * 那么我们就需要去研究下sqlSessionTemplate是什么初始化的
      * 我们知道spring 跟mybatis整合的时候，进行了偷天换日
      * 把我们mapper接口包下的所有接口类型都变为了MapperFactoryBean
      * 然后我们发现实现了SqlSessionDaoSupport,我们还记得在整合的时候，
-     * 把我们EmployeeMapper(案例class类型属性为MapperFactoryBean)
+     * 把我们 EmployeeMapper(案例class类型属性为MapperFactoryBean)
      * 的注入模型给改了，改成了by_type,所以会调用SqlSessionDaoSupport
      * 的setXXX方法进行赋值,从而创建了我们的sqlSessionTemplate
      * 而在实例化我们的sqlSessionTemplate对象的时候，为我们创建了sqlSessionTemplate的代理对象
      *     this.sqlSessionProxy = (SqlSession) newProxyInstance(SqlSessionFactory.class.getClassLoader(),
-            new Class[] { SqlSession.class }, new SqlSessionInterceptor());
+     *     new Class[] { SqlSession.class }, new SqlSessionInterceptor());
      */
     return mapperMethod.execute(sqlSession, args);
   }
@@ -129,7 +126,7 @@ public class MapperProxy<T> implements InvocationHandler, Serializable {
   }
 
   private Object invokeDefaultMethod(Object proxy, Method method, Object[] args)
-      throws Throwable {
+    throws Throwable {
     final Class<?> declaringClass = method.getDeclaringClass();
     final Lookup lookup;
     if (lookupConstructor.getParameterCount() == 2) {
