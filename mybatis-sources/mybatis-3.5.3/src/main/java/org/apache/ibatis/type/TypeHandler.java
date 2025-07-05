b94123b0cce77a8e1204cb07f5a1268886dea4ff
wanjunjie <ogo1828wan@163.com>
2025-07-05 16:22:31 +0800

    学习mybatis-spring源码

--- mybatis-sources/mybatis-3.5.3/src/main/java/org/apache/ibatis/type/TypeHandler.java
+++ mybatis-sources/mybatis-3.5.3/src/main/java/org/apache/ibatis/type/TypeHandler.java
@@ -25,10 +25,24 @@ import java.sql.SQLException;
  */
 public interface TypeHandler<T> {
 
+  /**
+   * 负责将Java类型转换为JDBC类型, 本质上执行的就是JDBC中的PreparedStatement的set方法
+   *  例如: String sql = "insert into user(name, age) values(?, ?)";
+   *       PreparedStatement ps = conn.prepareStatement(sql);
+   *       ps.setString(1, "张三");
+   *       ps.setInt(2, 18);
+   * @param ps
+   * @param i 对应占位符的位置
+   * @param parameter 占位符对应的值
+   * @param jdbcType 对应的数据库类型
+   */
   void setParameter(PreparedStatement ps, int i, T parameter, JdbcType jdbcType) throws SQLException;
 
   /**
-   * @param columnName Colunm name, when configuration <code>useColumnLabel</code> is <code>false</code>
+   * 从ResultSet中获取数据时会调用此方法,会将数据从JdbcType转换为Java类型,本质上执行的就是JDBC中的ResultSet的get方法
+   *  例如: ResultSet rs = ps.executeQuery();
+   *       String name = rs.getString("name");
+   *       int age = rs.getInt("age");
    */
   T getResult(ResultSet rs, String columnName) throws SQLException;
 
